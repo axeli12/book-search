2e5d17f0a3f0c1b99f0631521d39c4b5d8d93970
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-06-10 12:53:54 -0600

    added mutations and query for searches

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client'
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!){
+        createUser(usernmae: $username, email: $email, password: $password){
+            token
+            user{
+                username
+                email
+            }
+        }
+    }
+`
+
+export const LOGIN_USER = gql`
+ mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user {
+            email
+            password
+        }
+    }
+ }
+`
+
+export const SAVE_BOOK = gql`
+ mutation saveBook($bookData: BookInput!){
+    saveBook(bookdata: $bookData){
+        _id
+        username
+        email
+        savedBooks{
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+    }
+ }
+`
+
+export const DELETE_BOOK = gql`
+ mutation deleteBook($bookId: ID!){
+    deleteBook(bookID: $bookId){
+        _id
+        username
+        email
+        savedBooks{
+            bookId
+            authors
+            description
+            image
+            link
+            title
+    }
+ }
+`
