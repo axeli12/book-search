3a4d274b44e52d97b5639a4ab07b7cdf4a6638be
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-06-10 12:23:59 -0600

    added apolloserver and graphql connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {typeDefs, resolvers} = require('./schemas')
+const {authMiddleware} = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +20,20 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'))
+})
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+}
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+  console.log(`Use Graphql at https://localhost:${PORT}${server.graphqlPath}`)
 });
+});
+
+startApolloServer();
